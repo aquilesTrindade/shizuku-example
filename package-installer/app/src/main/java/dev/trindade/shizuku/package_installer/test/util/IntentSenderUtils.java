c42e70a88f804d274de0bc235255916693c3a9de
aquilesTrindade <sempedra075@gmail.com>
2024-06-17 06:22:44 -0300

    test: use shizuku demo

---
+++ package-installer/app/src/main/java/dev/trindade/shizuku/package_installer/test/util/IntentSenderUtils.java
@@ -0,0 +1,14 @@
+package dev.trindade.shizuku.package_installer.test.util;
+
+import android.content.IIntentSender;
+import android.content.IntentSender;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class IntentSenderUtils {
+
+    public static IntentSender newInstance(IIntentSender binder) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        //noinspection JavaReflectionMemberAccess
+        return IntentSender.class.getConstructor(IIntentSender.class).newInstance(binder);
+    }
+}
