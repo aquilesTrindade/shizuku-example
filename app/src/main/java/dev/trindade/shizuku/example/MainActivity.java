969f495200e3254fc3562db16d55d5c6c93ff86b
devsuay <devsuay@gmail.com>
2024-06-17 03:52:30 -0300

    add toast import

--- app/src/main/java/dev/trindade/shizuku/example/MainActivity.java
+++ app/src/main/java/dev/trindade/shizuku/example/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast; 
 import android.content.pm.PackageManager;
 
 import rikka.shizuku.Shizuku;
