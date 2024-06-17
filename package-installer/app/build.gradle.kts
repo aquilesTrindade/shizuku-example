599c02d4515d65c53801247cfa055e73867b87c5
aquilesTrindade <sempedra075@gmail.com>
2024-06-17 12:52:56 -0300

    fixes

--- package-installer/app/build.gradle.kts
+++ package-installer/app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     id("com.android.application")
     id("dev.rikka.tools.refine")
@@ -7,45 +6,50 @@ plugins {
 android {
     namespace = "dev.trindade.shizuku.package_installer"
     compileSdk = 34
-    
+
     defaultConfig {
         applicationId = "dev.trindade.shizuku.package_installer"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        
-        vectorDrawables { 
+
+        vectorDrawables {
             useSupportLibrary = true
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
     buildFeatures {
         viewBinding = true
-        
+        aidl = true
     }
-    
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+        }
+    }
 }
 
 dependencies {
-
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.9.0")
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("dev.rikka.shizuku:provider:13.1.5")
-
 }
