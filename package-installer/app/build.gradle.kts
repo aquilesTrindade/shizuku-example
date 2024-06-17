4c2993d828223a15c8464830ae65ed7753e00247
aquilesTrindade <sempedra075@gmail.com>
2024-06-17 07:31:03 -0300

    fixes

--- package-installer/app/build.gradle.kts
+++ package-installer/app/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     id("com.android.application")
-    
+    id("dev.rikka.tools.refine")
 }
 
 android {
--- package-installer/build.gradle.kts
+++ package-installer/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
-         
+    id("dev.rikka.tools.refine") version "4.4.0" apply false
 }
 
 tasks.register<Delete>("clean") {
