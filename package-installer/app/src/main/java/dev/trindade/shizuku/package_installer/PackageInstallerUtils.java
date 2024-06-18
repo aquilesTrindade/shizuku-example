c445c30479ab9c0bdfd4aae14bfe5b81ae8606f4
devsuay <devsuay@gmail.com>
2024-06-17 22:05:06 -0300

    fix

--- package-installer/app/src/main/java/dev/trindade/shizuku/package_installer/PackageInstallerUtils.java
+++ package-installer/app/src/main/java/dev/trindade/shizuku/package_installer/PackageInstallerUtils.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInstaller;
-import android.util.//Log;
 import android.widget.Toast;
 
 import java.io.File;
